905aafeea2b81f15ef6ff5a0a9eb1ad2f7dbd9ee
Ilya Kanishev <kanishev2002@gmail.com>
2021-01-11 19:44:23 +0300

    Added a wrapper for app subscribed to Redux store

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,16 +1,83 @@
-import react.*
-import react.dom.*
+import kotlinx.browser.document
 import kotlinx.html.js.onClickFunction
+import org.w3c.dom.HTMLInputElement
 import org.w3c.dom.events.Event
+import react.*
+import react.dom.button
+import react.dom.div
+import react.dom.h1
+import react.redux.provider
+import react.redux.rConnect
+import redux.RAction
+import redux.WrapperAction
+import redux.createStore
+import redux.rEnhancer
+import kotlin.random.Random
 
 external interface AppState : RState {
     var currentId: Int
     var inputNameValue: String
     var inputDescriptionValue: String
     var tasks: List<Task>
+    var onClick: () -> Unit
 }
 
-class App : RComponent<RProps, AppState>() {
+
+class AddTask(val task: Task) : RAction
+
+private fun appReducer(state: Array<Task> = emptyArray(), action: RAction): Array<Task> = when (action) {
+    is AddTask -> state + action.task
+    else -> state
+}
+
+private val store = createStore(::appReducer, emptyArray(), rEnhancer())
+object IdGenerator {
+    var id = 1
+    fun get() = id++
+}
+
+private interface AppStateProps : RProps {
+    var tasks: Array<Task>
+}
+
+private interface AppDispatchProps : RProps {
+    var onClick: () -> Unit
+}
+
+val connectedApp = rConnect<Array<Task>, AddTask, WrapperAction, RProps, AppStateProps, AppDispatchProps, AppProps>(
+    { state, _ ->
+        tasks = state
+    },
+    { dispatch, _ ->
+        onClick = {
+            val taskName = (document.getElementById(
+                "Name input field"
+            ) as? HTMLInputElement)?.value
+            val taskDescription = (document.getElementById(
+                "Description input field"
+            ) as? HTMLInputElement)?.value ?: ""
+            if(!taskName.isNullOrEmpty()) {
+                dispatch(
+                    AddTask(
+                        Task(
+                            IdGenerator.id,
+                            taskName,
+                            taskDescription,
+                            false
+                            )
+                    )
+                )
+            }
+        }
+    }
+)(App::class.js.unsafeCast<RClass<AppProps>>())
+
+external interface AppProps : RProps {
+    var tasks: Array<Task>
+    var onClick: () -> Unit
+}
+
+class App : RComponent<AppProps, RState>() {
     private fun handleSubmitButton() {
         if (!state.inputNameValue.isNullOrEmpty()) {
             setState {
@@ -46,45 +113,49 @@ class App : RComponent<RProps, AppState>() {
     }
 
     override fun RBuilder.render() {
-        div(classes = "appHeader") {
-            h1(classes = "headerText") {
-                +"TODO List"
-            }
-        }
-        div(classes = "app") {
-            div(classes = "taskList") {
-                // TODO: actually implement tasks
-                // val tasks = state.tasks
-                child(TaskList::class) {
-                    attrs.tasks = listOf(
-                        Task(1, "task1", "task description", false),
-                        Task(2, "task2", "another task description", true)
-                    )
+        provider(store) {
+            div(classes = "appHeader") {
+                h1(classes = "headerText") {
+                    +"TODO List"
                 }
             }
-            div(classes = "inputFields") {
-                child(InputField::class) {
-                    attrs {
-                        value = state.inputNameValue
-                        onChange = { handleNameInput(it) }
-                        placeholder = "Input name of the task"
+            div(classes = "app") {
+                div(classes = "taskList") {
+                    // TODO: actually implement tasks
+                    // val tasks = state.tasks
+                    child(TaskList::class) {
+                        attrs.tasks = listOf(
+                            Task(1, "task1", "task description", false),
+                            Task(2, "task2", "another task description", true)
+                        )
                     }
                 }
-                child(InputField::class) {
-                    attrs {
-                        value = state.inputDescriptionValue
-                        onChange = { handleDescriptionInput(it) }
-                        placeholder = "Input task description"
-                    }
-                }
-                div("submit-button-area") {
-                    button(classes = "submit-button") {
+                div(classes = "inputFields") {
+                    child(InputField::class) {
                         attrs {
-                            onClickFunction = {
-                                handleSubmitButton()
-                            }
+                            //value = state.inputNameValue
+                            //onChange = { handleNameInput(it) }
+                            id = "Name input field"
+                            placeholder = "Input name of the task"
                         }
-                        +"Submit"
+                    }
+                    child(InputField::class) {
+                        attrs {
+                            //value = state.inputDescriptionValue
+                            //onChange = { handleDescriptionInput(it) }
+                            id = "Description input field"
+                            placeholder = "Input task description"
+                        }
+                    }
+                    div("submit-button-area") {
+                        button(classes = "submit-button") {
+                            attrs {
+                                onClickFunction = {
+                                    // TODO
+                                }
+                            }
+                            +"Submit"
+                        }
                     }
                 }
             }
--- src/main/kotlin/InputField.kt
+++ src/main/kotlin/InputField.kt
@@ -1,26 +1,38 @@
+import kotlinx.html.id
 import kotlinx.html.js.onChangeFunction
-import org.w3c.dom.events.Event
+import org.w3c.dom.HTMLInputElement
 import react.*
 import react.dom.*
 
 external interface InputFieldProps : RProps {
-    var value: String
-    var onChange: (event: Event) -> Unit
+    //var value: String
+    //var onChange: (event: Event) -> Unit
+    var id: String
     var placeholder: String
 }
 
+external interface InputFieldState : RState {
+    var value: String?
+}
 
-class InputField : RComponent<InputFieldProps, RState>() {
+
+
+class InputField : RComponent<InputFieldProps, InputFieldState>() {
     override fun RBuilder.render() {
         div(classes = "inputField") {
             input(classes = "inputBorder") {
                 attrs {
-                    value = props.value
+                    id = props.id
+                    value = state.value ?: ""
                     placeholder = props.placeholder
-                    onChangeFunction = props.onChange
+                    onChangeFunction = {
+                        val value = (it.target as? HTMLInputElement)?.value
+                        setState {
+                            this.value = value
+                        }
+                    }
                 }
             }
         }
     }
-
 }
